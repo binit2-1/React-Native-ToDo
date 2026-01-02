05d6470e64c7a97eacb8af0280365d608d9b2e85
binit <binitgupta.1711@gmail.com>
2026-01-02 12:56:57 +0530

    Swipe Onboarding Working

--- client/app/(root)/(auth)/Welcome.tsx
+++ client/app/(root)/(auth)/Welcome.tsx
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { View} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Button } from "../../../components/ui/button";
 import { Text } from "../../../components/ui/text";
+import { router } from "expo-router";
+import Swiper from 'react-native-swiper';
+import {onboarding} from '../../../constants/index'
 
 const Welcome = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+
   return (
-    <SafeAreaView className="flex-1">
-      <View className="flex-1 items-center justify-center bg-black">
-        <Button>
+    <SafeAreaView className="flex h-full w-full items-center justify-between bg-background">
+      <Swiper ref={swiperRef} loop={false}
+       dot={<View className="bg-muted/50 mx-1 h-2 w-2 rounded-full" />}
+       activeDot={<View className="bg-foreground mx-1 h-2 w-2 rounded-full" />}
+       onIndexChanged={(index)=>{
+        setActiveIndex(index)
+       }}>
+        {onboarding.map((item)=>(
+          <View key={item.id} className="flex justify-between items-center">
+            <Text className="font-google-sans-flex-9pt-medium">{item.title}</Text>
+          </View>
+        ))}
+      </Swiper>
+      <View className="flex flex-row gap-4">
+        <Button onPress={() => {
+          router.replace("/Register")
+        }} 
+        className="font-google-sans-flex-9pt-medium bg-foreground items-center">
           <Text className="font-google-sans-flex-9pt-medium">
-            Welcome
+            Skip
+          </Text>
+        </Button>
+        <Button onPress={() => {
+          router.replace("/Register")
+        }} 
+        className="font-google-sans-flex-9pt-medium bg-foreground items-center">
+          <Text className="font-google-sans-flex-9pt-medium">
+            Next
           </Text>
         </Button>
       </View>
---
+++ client/constants/index.ts
@@ -0,0 +1,20 @@
+export const onboarding = [
+    {
+        id:1,
+        title:"Capture Every Idea",
+        desc: "Never let a task slip away. Quickly add to-dos as they come to you.",
+        image: null
+    },
+    {
+        id: 2,
+        title: "Stay on Target",
+        desc: "Watch your progress grow. Categorize and manage tasks with ease.",
+        image: null
+    },
+    {
+        id: 3,
+        title: "Never Miss a Beat",
+        desc: "Get timely reminders so you're always ahead of your schedule.",
+        image: null
+    }
+]
